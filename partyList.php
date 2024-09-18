58ffff6b420a63dcb98f670e47df94d5639f8c9b
Shalin Devpura <shalindevpura19@gmail.com>
2024-09-17 22:40:09 -0700

    server update

--- partyList.php
+++ partyList.php
@@ -11,12 +11,22 @@ include 'db.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        #partyForm tr,
+        #partyForm td {
+            border: 0px !important;
+        }
+
+        #partyForm {
+            border: 1px solid rgba(0,0,0,0.1);
+        }
+    </style>
 </head>
 
 <body>
     <?php include 'navbar.php'; ?>
     <div class="container mt-5">
-        <form action="./partyModify" method="post">
+        <form id="partyForm" action="./partyModify" method="post">
             <table class="table table-bordered">
                 <tr>
                     <td><label for="ID">Customer ID</label></td>
@@ -71,7 +81,7 @@ include 'db.php';
                     <th>GST/PAN</th>
                     <th>Edit</th>
                     <th>Delete</th>
-                    <th>Inactive/Active</th>
+                    <th>Inactive/<br>Active</th>
                 </tr>
             </thead>
             <tbody>
